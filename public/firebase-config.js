dd779aa3d42bdf065f6414f4c498f4290674dac8
SeungHyunKimLG <kiemsh627@gmail.com>
2026-06-24 01:49:39 +0900

    Add Firebase configuration file

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAUBNCwvwbj95Q8np-6TUoh9WctRFkyrcY",
+  authDomain: "pump-management-99364.firebaseapp.com",
+  projectId: "pump-management-99364",
+  storageBucket: "pump-management-99364.firebasestorage.app",
+  messagingSenderId: "515406280245",
+  appId: "1:515406280245:web:9abddfaa1c3976ff78237c"
+};
