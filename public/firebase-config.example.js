9f78e36102786bd90232aeabd5d512e540c0967b
SeungHyunKimLG <kiemsh627@gmail.com>
2026-06-24 01:49:49 +0900

    Add example Firebase configuration file

---
+++ public/firebase-config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIza...",
+  authDomain: "pump-management-99364.firebaseapp.com",
+  projectId: "pump-management-99364",
+  storageBucket: "pump-management-99364.firebasestorage.app",
+  messagingSenderId: "515406280245",
+  appId: "1:515406280245:web:..."
+};
